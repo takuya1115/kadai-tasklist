402ebad4da225838ba12343ea6977e9ce40be92e
takuya.maeda <daema1115@icloud.com>
2020-11-29 07:02:42 +0000

    kadai-1129-2

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -59,6 +59,7 @@ class TasksController extends Controller
         $task=new Task;
         $task->content=$request->content;
         $task->status=$request->status;
+        $task->user_id=\Auth::id();
         $task->save();
         
         return redirect("/");
@@ -106,6 +107,7 @@ class TasksController extends Controller
         $task=Task::findOrFail($id);
         $task->content=$request->content;
         $task->status=$request->status;
+        
         $task->save();
         
         return redirect("/");
